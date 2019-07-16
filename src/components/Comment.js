b517a26bdbe9541abe861a09c11cfd560ff11cb1
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-07-16 00:35:39 -0300

    chore: Add timeline
    I really need to improve my css skills 🤔

---
+++ src/components/Comment.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Comment({ comment }) {
+  const { author, content } = comment;
+
+  return (
+    <div className='comment'>
+      <img src={author.avatar} alt='Avatar' width='28' height='28' />
+
+      <p><strong>{author.name}</strong> {content}</p>
+    </div>
+  );
+}
+
+Comment.propTypes = {
+  comment: PropTypes.shape({
+    id: PropTypes.number,
+    author: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.element.isRequired,
+    }).isRequired,
+    content: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+Comment.defaultProps = {};
+
+export default Comment;
---
+++ src/components/Post.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Comment from './Comment';
+
+function Post({ post }) {
+  const { author, date, content, comments } = post;
+
+  return (
+    <section className='card'>
+      <img src={author.avatar} alt='Avatar' />
+
+      <div className='info'>
+        <h3>{author.name}</h3>
+        <time>{date}</time>
+      </div>
+
+      <div>
+        <p>{content}</p>
+
+        {comments.map(comment => <Comment comment={comment} />)}
+      </div>
+    </section>
+  );
+}
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    author: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.element.isRequired,
+    }).isRequired,
+    date: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    comments: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      author: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        avatar: PropTypes.element.isRequired,
+      }).isRequired,
+      content: PropTypes.string.isRequired,
+    })).isRequired,
+  }).isRequired,
+};
+
+Post.defaultProps = {};
+
+export default Post;
